7b5326946a348a58660fc015b5b36974df73c1f2
xuniorss <fortunatojunior2020@gmail.com>
2023-08-26 19:14:34 -0300

    feat: Prisma & Planetscale setup

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -6,6 +6,7 @@ type MyVariables = {
 	NEXT_PUBLIC_CLERK_SIGN_UP_URL: string
 	NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: string
 	NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: string
+	DATABASE_URL: string
 }
 
 declare global {
