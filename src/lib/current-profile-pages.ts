0dcbef02d9fbe480e5a4487627f0c06413981f9a
xuniorss <fortunatojunior2020@gmail.com>
2023-08-29 21:31:09 -0300

    feat: Messages API

---
+++ src/lib/current-profile-pages.ts
@@ -0,0 +1,12 @@
+import { getAuth } from '@clerk/nextjs/server'
+
+import { NextApiRequest } from 'next'
+import { db } from './db'
+
+export const currentProfilePages = async (req: NextApiRequest) => {
+	const { userId } = getAuth(req)
+
+	if (!userId) return null
+
+	return await db.profile.findUnique({ where: { userId } })
+}
